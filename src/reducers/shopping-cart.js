27b7933991d03a56ec16dc553f1d3113e7273ae1
ArtyomSavchenkoV <41994318+ArtyomSavchenkoV@users.noreply.github.com>
2019-04-09 12:12:03 +0700

    Add comment into shopping-cart.js

--- src/reducers/shopping-cart.js
+++ src/reducers/shopping-cart.js
@@ -1,25 +1,100 @@
-const updateCart = (cartItems, cartItemIndex, cartItem) => {
+/**
+ * a part of reducer, responsible for part of store - shoppingCart.
+ * 
+ * Action splits on three parts: 
+ * add one book, remove one book, and remove all books from row.
+ * All this actions performs in `updateOrder` function via giving
+ * quantity parameter, which can be: 
+ * `1` for add one book
+ * `-1` for remove one book 
+ * `-item.count` for remove all books
+ * 
+ * The function `updateOrder` prepares data and starts functions:
+ * `updateCartItem`
+ * `updateCartArray`
+ * After this returns ready object `shoppingCart`
+ * 
+ * The function `updateCartItem` updates, or creates and returns ready object `cartItem` anyway.
+ * `object.count` changes according to the `quantity` parameter.
+ * 
+ * The function `updateCartArray` gets new `cartItem` from function `updateCartItem`
+ * and if `cartItem.count` is 0, then this `cartItem` removes from `cartItems`
+ * if `cartItems` not contains `cartItem` object of additional book, then `cartItem` inserts into `cartItems`.
+ * else update `cartItem` in `cartItems`.
+ * 
+ * @param state
+ * @param action
+ * 
+ * @return {object} new shoppingCart
+ * shoppingCart structure:
+ {
+ 	cartItems: [
+ 		{
+			id,
+			title,
+			count,
+			total
+		},
+		{
+			id,
+			title,
+			count,
+			total
+		}
+	],
+	orderTotal
+}
+ */
+
+
+/**
+ * create an updated shopping cart array, called `cartItems`. 
+ * This function is intended for creating, updating or removing items.
+ * 
+ * @param {array of objects} previous objects cartItems.
+ * @param {int} index of cart item. If index less than zero then add new cart item record.
+ * @param {object} ready cart item. If `.count < 1` then delete record.
+ * 
+ * @return {array of objects} new cartItems
+ */
+const updateCartArray = (cartItems, cartItemIndex, cartItem) => {
+	//delete cart item record, because its count of books became 0
     if (cartItem.count < 1) {
         return [
             ...cartItems.slice(0, cartItemIndex),
             ...cartItems.slice(cartItemIndex+1)
         ]
     }
-    if (cartItemIndex > -1) {
+    
+    //insert new book
+    if (cartItemIndex < 0) {
         return [
+			...cartItems,
+			cartItem
+		];
+    }
+    
+    //update cart item
+	return [
             ...cartItems.slice(0, cartItemIndex),
             cartItem,
             ...cartItems.slice(cartItemIndex+1)
-        ];
-    }
-
-    return [
-        ...cartItems,
-        cartItem
-    ];
+        ];    
 };
 
+/**
+ * update cart item or create a cart item.
+ * 
+ * @param {object, undefined} previous cart item.
+ * @param {object} book from bookList
+ * @param {int} quantity of additional books, 
+ * 		negative value decreaces count
+ * 
+ * @return {object} new cart item
+ */
 const updateCartItem = (cartItem = {}, book, quantity) => {
+	
+	//if cartItem was empty, then fill fields for create new cart item
     const {
         id = book.id,
         title = book.title,
@@ -34,15 +109,34 @@ const updateCartItem = (cartItem = {}, book, quantity) => {
     }
 };
 
+/**
+ * calculate common order total
+ * 
+ * @param {arary of objects} shopping cart.
+ * 
+ * @return {number} amount of object.total
+ */
 const mathOrderTotal = (newCart) => {
     return newCart.reduce((result, {total}) => {
         return result + total
     }, 0);
 };
 
+/**
+ * prepare data for update order
+ * 
+ * @param {object} state
+ * @param {int} book ID
+ * @param {int} quantity of additional books, 
+ * 		possitive value increases count
+ * 		negative value decreaces count
+ * 
+ * @return {object} shoppingCart
+ */
 const updateOrder = ({shoppingCart, bookList}, bookId, quantity) => {
     const { books } = bookList;
     const { cartItems } = shoppingCart;
+    
     const book = books.find(({ id }) => id === bookId);
     const cartItemIndex = cartItems.findIndex(({ id }) => id === bookId);
 
@@ -60,7 +154,16 @@ const updateOrder = ({shoppingCart, bookList}, bookId, quantity) => {
     }
 };
 
+/**
+ * a part of reducer, responsible for part of store - shoppingCart.
+ * 
+ * @param {object} state
+ * @param {object} action
+ * 
+ * @return {object} shoppingCart
+ */
 const updateShoppingCart = (state, action) => {
+	//Create shoppingCart
     if (state === undefined) {
         return {
             cartItems: [],
