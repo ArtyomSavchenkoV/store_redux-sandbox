519d0bafa689daf0bc76e8f835bf8b9ad20ce07d
ArtyomSavchenkoV <41994318+ArtyomSavchenkoV@users.noreply.github.com>
2019-04-09 12:17:56 +0700

    Add comment into main reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,16 @@
 import updateBookList from './book-list';
 import updateShoppingCart from './shopping-cart'
 
+/**
+ * Reducer splits state on book list and shopping cart.
+ * And deligates actions them.
+ * 
+ * @param state
+ * @param action
+ * 
+ * @return new state
+ */
+
 const reducer = (state, action) => {
     return {
         bookList: updateBookList(state, action),
