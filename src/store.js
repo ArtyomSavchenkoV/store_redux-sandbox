296eeff400042c9557db3a27eb6f6179f193d982
ArtyomSavchenkoV <41994318+ArtyomSavchenkoV@users.noreply.github.com>
2019-04-09 12:15:39 +0700

    add comment into store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 
-
 import reducer from './reducers';
 
+/**
+ * Use string as action, and dispatch it.
+ * 
+ * @param of external function {} get Store. In this example not use.
+ * @param of next function {function} dispatch.
+ * @param of internal function {object or string} action object or
+ * 		reduce action string
+ * 
+ * @return dispatched action
+ */
 const stringMiddleware = (/*store*/) => (dispatch) => (action) => {
     if(typeof action === 'string') {
         return dispatch({
@@ -13,6 +22,9 @@ const stringMiddleware = (/*store*/) => (dispatch) => (action) => {
     return dispatch(action)
 };
 
+/**
+ * Insert "action type" string into the log.
+ */
 const logMiddleware = (/*store*/) => (dispatch) => (action) => {
     console.log(action.type/*, store.getState()*/);
     return dispatch(action);
